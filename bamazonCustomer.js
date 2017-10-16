7fc317ed8410b1e50608e90e48a21f716c0a23ed
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-10-16 16:31:36 -0600

    rtf

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -37,7 +37,7 @@ function displayProducts(){
       // if (err) throw err;
       for (i = 0; i < res.length; i++) {
 // console.log([res[i].item_id, res[i].product_name, res[i].department_name, + "$" + res[i].retail_price, res[i].stock_quantity]);
-        table.push([res[i].item_id, res[i].product_name, res[i].department_name, '$' + res[i].retail_price.toFixed(2), res[i].stock_quantity]);
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, '$' + res[i].retail_price.toFixed(2), res[i].stock_quantity, res[i].remaining_inventory]);
       }
 
       console.log(table.toString());
@@ -73,31 +73,83 @@ function iWantToBuy() {
             .then(function(answers){
               // var idOfPurchase = answers.items;
               var quan = answers.quantity;
-              var purchaseCheck = 'SELECT item_id, product_name, stock_quantity, retail_price FROM products where ?';
+              var quan1 = parseInt(quan);
+              var purchaseCheck = 'SELECT item_id, product_name, stock_quantity, sold_inventory, retail_price, remaining_inventory FROM products where ?';
                 connection.query(purchaseCheck, {item_id: answers.items}, function(err, res){
                 // if (err) throw err;
+                  var productID = res[0].item_id;
                   var stock = res[0].stock_quantity;
                   var retail_price = res[0].retail_price;
                   var product_name = res[0].product_name;
                   var product_sales = res[0].sold_inventory;
                   var department_name = res[0].department_name;
+                  var remaining_inventory = res[0].remaining_inventory;
 
-                 if (quan <= stock) {
+                 if (quan <= remaining_inventory) {
                    var yourCost1 = quan * retail_price;
                    console.log(" ");
                    console.log('-------------------------------------------------------------------------------------');
                     console.log('\n  Great news! We have your item in stock and will process your order immediately. \n');
                     console.log('-------------------------------------------------------------------------------------');
-                      console.log(' The total cost for \n' + ' (' + quan + ')' + "\n " + product_name + " \n is " + "$" + yourCost1 + ".");
-                      inquirer.prompt([{
-                        name: "confirmation",
-                        type: "confirm",
-                        message: "Please confirm that you agree to purchase " + '(' + quan + ')' + "\n" + product_name + " \n for " + "$" + yourCost1 + "."
-                      }]).then(function(user){
-                        if(user.confirmation === "yes"){
-                          console.log(' Thank you for supporting Bamazon\'s online storefront.\n You will receive an email shortly regarding payment and shipping options. ');
-                          console.log('\n-------------------------------------------------------------------------------------');
-                          inquirer.prompt([{
+                    console.log(' The total cost for \n' + ' (' + quan + ')' + "\n " + product_name + " \n is " + "$" + yourCost1 + ".");
+                    inquirer.prompt([{
+                      name: "confirmation",
+                      type: "list",
+                      message: "Please confirm that you agree to purchase " + '(' + quan + ')' + "\n" + product_name + " \n for " + "$" + yourCost1 + ". Please select 1 or 2.",
+                      choices: ['Yes', 'No']
+                    }]).then(function(user){
+                      if(user.confirmation == "Yes"){
+                          connection.query('UPDATE products SET sold_inventory = sold_inventory - ' + quan + ' WHERE item_id = ' + productID);
+                            connection.query('UPDATE products SET remaining_inventory = stock_quantity + sold_inventory WHERE item_id = ' + productID);
+                        console.log(' Thank you for supporting Bamazon\'s online storefront.\n You will receive an email shortly regarding payment and shipping options. ');
+                        console.log('\n-------------------------------------------------------------------------------------');
+                          console.log("Sold inventory has been updated");
+                        }
+                      }
+                  );
+                }
+
+                    else {
+                      console.log(" ");
+                      console.log('-------------------------------------------------------------------------------------');
+                      console.log(' We apologize but the the quantity of purchase exceeds our available inventory.');
+                      console.log('\n We have ' + remaining_inventory + " " + product_name + " \n in stock.");
+                      console.log('-------------------------------------------------------------------------------------');
+                      console.log(" ");
+                        if(remaining_inventory === 0){
+                          console.log(" ");
+                          console.log('-------------------------------------------------------------------------------------');
+                          console.log("\n We will return you to our inventory list to select another product.");
+                          setTimeout(function() { displayProducts(); }, 6000);
+                          console.log(" ");
+                          console.log('-------------------------------------------------------------------------------------');
+
+                        }
+
+
+                        else{
+
+                    inquirer.prompt([{
+                        name: "stillPurchase",
+                        type: "list",
+                        message: "Would you like to purchase the " + remaining_inventory + " unit(s) we have in stock? Please select 1 or 2",
+                        choices: ['Yes','No']
+                      }])
+                      .then(function(user){
+                          if(user.stillPurchase === 'Yes') {
+                            var yourCost2 = remaining_inventory * retail_price;
+                            console.log('-------------------------------------------------------------------------------------');
+                              console.log(' Perfect! The total cost for \n' + ' (' + remaining_inventory + ')' + "\n " + product_name + " \n is " + "$" + yourCost2 + ".");
+                              console.log('\n-------------------------------------------------------------------------------------');
+                              console.log(' Thank you for supporting Bamazon\'s online storefront.\n You will receive an email shortly regarding payment and shipping options. ');
+                              console.log('\n-------------------------------------------------------------------------------------');
+                              connection.end();
+
+                        }
+                      });
+
+                          function inquirerPrompt(){
+                            inquirer.prompt([{
                             name: "choice",
                             type: "list",
                             message: "What would you like to do next?",
@@ -105,121 +157,19 @@ function iWantToBuy() {
                           }])
                           .then(function(user){
                             if(user.choice !== 'Return to Bamazon\'s inventory list') {
-                              console.log(' OK. Thank you for visting Bamazon\'s online storefront. Please come again to shop soon.');
+                              console.log(' OK. Thank you for visting Bamazon\'s online storefront. Please shop with us soon.');
                               connection.end();
-                            }else {
-                              displayProducts();
-
-                        }
-                    }
-
-                    else {
-                      console.log(" ");
-                      console.log('-------------------------------------------------------------------------------------');
-                      console.log(' We apologize but the the quantity of purchase exceeds our available inventory.');
-                      console.log('\n We have ' + stock + " " + product_name + " \n in stock.");
-                      console.log('-------------------------------------------------------------------------------------');
-                      console.log(" ");
-                    }
-                    inquirer.prompt([{
-                        name: "stillPurchase",
-                        type: "list",
-                        message: "Would you like to purchase the " + stock + " unit(s) we have in stock?",
-                        choices: ['Yes','No']
-                      }])
-                      .then(function(user){
-                          if(user.stillPurchase === 'Yes') {
-                            var yourCost2 = stock * retail_price;
-                            console.log('-------------------------------------------------------------------------------------');
-                              console.log(' Perfect! The total cost for \n' + ' (' + stock + ')' + "\n " + product_name + " \n is " + "$" + yourCost2 + ".");
-                              console.log('\n-------------------------------------------------------------------------------------');
-                              console.log(' Thank you for supporting Bamazon\'s online storefront.\n You will receive an email shortly regarding payment and shipping options. ');
-                              console.log('\n-------------------------------------------------------------------------------------');
-                              connection.end();
-                        }
-                          else {
-                            inquirer.prompt([{
-                              name: "choice",
-                              type: "list",
-                              message: "What would you like to do next?",
-                              choices: ['Return to Bamazon\'s inventory list', 'End your online session with Bamazon?']
-                            }])
-                            .then(function(user){
-                              if(user.choice !== 'Return to Bamazon\'s inventory list') {
-                                console.log(' OK. Thank you for visting Bamazon\'s online storefront. Please come again to shop soon.');
-                                connection.end();
-                              }else {
-                                displayProducts();
+                            }
+                            else {
+                              iWantToBuy();
                             }
                           });
-                          }
-                        });
-                      }
-                    });
 
-              }
+                        }
+
+
+                  }
+                }
+              });
             });
-        });
-}
-                            // console.log('\n -----------------------------------------------------------------------------------------------\n\r OK. We apologize for this inconvenience. What would you like to do next?');
-//                             //  console.log('\n -----------------------------------------------------------------------------------------------\n\r ');
-//
-//                           inquirer.prompt([{
-//                             name: "cartOrEnd",
-//                             type: "list",
-//                             message: " What would you like to do next?\n Would you like to checkout now, or would you like to keep shopping?\n   Select 1 or 2, and then hit ENTER",
-//                             choices: ['Checkout Now', new inquirer.Separator(), 'Keep Shopping']
-//                           }])
-//                           .then(function(user){
-//                             if (user.cartOrEnd === 'Checkout Now') {
-//                               setTimeout(function(){
-//                                 console.log(" ");
-//                                 console.log('-------------------------------------------------------------------------------------');
-//                                 console.log(' The total cost for \n' + ' (' + quan + ')' + "\n " + product_name + " \n is " + "$" + yourCost + ".");
-//                               },2000);
-//                               // makePurchaseNow();
-//                             }
-//                             else {
-//                               displayProducts();
-//                           }
-//
-//
-//
-
-//                          .then(function(user){
-
-//
-//             }
-//           });
-//         }
-//       });
-//     });
-//   });
-// });
-//
-// }
-
-                        //  console.log(' OK. We will process your order now. ');
-                        //   var yourUnitCost = stock * retail_price;
-                        //     console.log('The total cost for  ' + stock + ' ' + product_name + ' is ' + '$' + yourUnitCost + ".");
-
-                      //  }
-                  //    });
-                  //  }
-                  //     else {
-                   //
-                  //            }
-                  //          });
-                   //
-                  //  }
-
-//       }else {}
-                            //  else {
-                            //    console.log('Thank you for stopping by Bamazon\'s online storefront. We hope you will come again soon.');
-
-
-
-                            //    connection.end();
-
-//
-//
+          }
